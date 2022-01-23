74a9f6bfcccc0933f1d16b1ad9ec403052a17b39
Alex Thalhammer <alex@angular-software.at>
2022-01-23 23:04:18 +0100

    05_r: Bonus: Loading Indicator *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  GuardsCheckEnd,
+  GuardsCheckStart,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World!';
+
+  showLoadingIndicator = false;
+
+  constructor(private router: Router) {
+    router.events.pipe(filter((e) => e instanceof NavigationStart || e instanceof GuardsCheckEnd)).subscribe((event) => {
+      this.showLoadingIndicator = true;
+    });
+
+    router.events
+      .pipe(
+        filter(
+          (e) =>
+            e instanceof NavigationEnd || e instanceof NavigationError || e instanceof NavigationCancel || e instanceof GuardsCheckStart
+        )
+      )
+      .subscribe((event) => {
+        this.showLoadingIndicator = false;
+      });
+  }
 }
--- src/app/flight-booking/flight-search/flight.resolver.ts
+++ src/app/flight-booking/flight-search/flight.resolver.ts
@@ -3,6 +3,7 @@ import { Flight } from '../flight';
 import { FlightService } from '../flight.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class FlightResolver implements Resolve<Flight> {
@@ -10,6 +11,6 @@ export class FlightResolver implements Resolve<Flight> {
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Flight> {
     const id = route.params.id;
-    return this.flightService.findById(id);
+    return this.flightService.findById(id).pipe(delay(3000)); // <-- delay!
   }
 }
