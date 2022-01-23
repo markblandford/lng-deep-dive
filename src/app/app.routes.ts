2d83ae463a3276658a3791a59c0eff7c9a3a3c2e
Alex Thalhammer <alex@angular-software.at>
2022-01-23 21:15:01 +0100

    05_r: Bonus: Aux Routing *

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { BasketComponent } from './basket/basket.component';
 
 export const APP_ROUTES: Routes = [
   {
@@ -18,6 +19,11 @@ export const APP_ROUTES: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'basket',
+    component: BasketComponent,
+    outlet: 'aux'
   },
   {
     path: '**',
