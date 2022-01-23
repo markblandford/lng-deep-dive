3ac516aab83fd8ed36aa1f9b0ded562436782a8b
Alex Thalhammer <alex@angular-software.at>
2022-01-23 16:37:13 +0100

    01_c: Bonus: Directly Accessing a ViewChild *

--- src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -1,14 +1,18 @@
-import { AfterContentInit, Component, ContentChildren, QueryList } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChildren, QueryList, ViewChild } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
+import { TabNavigatorComponent } from '../tab-navigator/tab-navigator.component';
 
 @Component({
   selector: 'app-tabbed-pane',
   templateUrl: './tabbed-pane.component.html',
   styleUrls: ['./tabbed-pane.component.scss']
 })
-export class TabbedPaneComponent implements AfterContentInit {
+export class TabbedPaneComponent implements AfterContentInit, AfterViewInit {
   @ContentChildren(TabComponent)
   tabQueryList: QueryList<TabComponent> | undefined;
+
+  @ViewChild('navigator')
+  navigator: TabNavigatorComponent | undefined;
 
   activeTab: TabComponent | undefined;
   currentPage = 1;
@@ -20,6 +24,17 @@ export class TabbedPaneComponent implements AfterContentInit {
   ngAfterContentInit(): void {
     if (this.tabs.length > 0) {
       this.activate(this.tabs[0]);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.navigator) {
+      this.navigator.pageCount = this.tabs.length;
+      // This line would cause a cycle:
+      // this.navigator.page = 1;
+      this.navigator.pageChange.subscribe((page: number) => {
+        this.pageChange(page);
+      });
     }
   }
 
