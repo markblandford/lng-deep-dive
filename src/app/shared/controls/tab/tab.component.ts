1d92a46246ac2c1b09a80ff01fd92d1564f28a2d
Alex Thalhammer <alex@angular-software.at>
2022-01-23 16:19:55 +0100

    01_c: Content Projection & Referencing the Parent Component

---
+++ src/app/shared/controls/tab/tab.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { TabbedPaneComponent } from '../tabbed-pane/tabbed-pane.component';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.scss']
+})
+export class TabComponent {
+  @Input() title = '';
+  visible = true;
+
+  constructor(pane: TabbedPaneComponent) {
+    pane.register(this);
+  }
+}
---
+++ src/app/shared/controls/tabbed-pane/tabbed-pane.component.ts
@@ -0,0 +1,29 @@
+import { AfterContentInit, Component } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabbed-pane',
+  templateUrl: './tabbed-pane.component.html',
+  styleUrls: ['./tabbed-pane.component.scss']
+})
+export class TabbedPaneComponent implements AfterContentInit {
+  tabs: TabComponent[] = [];
+  activeTab: TabComponent | undefined;
+
+  ngAfterContentInit(): void {
+    if (this.tabs.length > 0) {
+      this.activate(this.tabs[0]);
+    }
+  }
+
+  register(tab: TabComponent): void {
+    this.tabs.push(tab);
+  }
+
+  activate(active: TabComponent): void {
+    for (const tab of this.tabs) {
+      tab.visible = tab === active;
+    }
+    this.activeTab = active;
+  }
+}
