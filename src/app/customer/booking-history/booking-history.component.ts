516aeb11c49f79abefbf7018399e4a34fb373192
Alex Thalhammer <alex@angular-software.at>
2022-08-31 12:03:37 +0200

    Showing ngTemplateOutlet and ngComponentOutlet

--- src/app/customer/booking-history/booking-history.component.ts
+++ src/app/customer/booking-history/booking-history.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { FlightSearchComponent } from '../../flight-booking/flight-search/flight-search.component';
+
 @Component({
   selector: 'app-booking-history',
   templateUrl: './booking-history.component.html',
   styleUrls: ['./booking-history.component.scss']
 })
 export class BookingHistoryComponent {
+  flightSearchComponent = FlightSearchComponent;
+
   delete(): void {
     console.debug('delete ...');
   }
